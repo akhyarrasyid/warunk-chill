d6cd82bce883a667bf27a7cde65f933c973ec4ab
akhyarrasyid <akhyarrasyid2005@gmail.com>
2024-09-10 23:48:27 +0700

    Create Django Project

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def show_main(request):
+    context = {
+        'app_name': 'warunk_now',
+        'name': 'Akhyar Rasyid Asy syifa',
+        'class_name': 'PBP - D'
+    }
+    return render(request, 'main.html', context)
